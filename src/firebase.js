5893ab71e0a51906c28e79dfba2660827c431c2f
AndreSA0912 <andresantiago506@gmail.com>
2024-08-13 11:23:01 -0700

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBji8VME0G4azfUqqJM3qQj4i0mSnha3Tg",
+  authDomain: "sistema-inventario-e4c62.firebaseapp.com",
+  projectId: "sistema-inventario-e4c62",
+  storageBucket: "sistema-inventario-e4c62.appspot.com",
+  messagingSenderId: "510368826440",
+  appId: "1:510368826440:web:186e662b1aa2068d93847d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
